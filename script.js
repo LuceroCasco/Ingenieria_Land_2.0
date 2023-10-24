975b4c52514d2f1bf5ddb12215676fef9420ded4
LuceroCasco <loooclas024@gmail.com>
2023-10-24 19:10:46 -0300

    New

--- script.js
+++ script.js
@@ -3,42 +3,54 @@ async function startQRScanner() {
     const qrResultElement = document.getElementById('qr-result');
 
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-        videoElement.srcObject = stream;
+        const hasCameraPermission = localStorage.getItem('cameraPermission') === 'granted';
+        let stream;
 
-        const worker = new Worker('jsQR.js');
-        worker.postMessage({ type: 'init' });
+        if (!hasCameraPermission) {
+            stream = await requestCameraPermission();
+        } else {
+            stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+        }
 
-        worker.onmessage = (e) => {
-            const result = e.data;
-            if (result.type === 'decoded') {
-                qrResultElement.textContent = `Resultado del escaneo: ${result.data}`;
-            }
-        };
+        if (stream) {
+            videoElement.srcObject = stream;
 
-        const onFrame = async () => {
-            if (videoElement.readyState === videoElement.HAVE_ENOUGH_DATA) {
-                const canvas = document.createElement('canvas');
-                const context = canvas.getContext('2d');
-                canvas.width = videoElement.videoWidth;
-                canvas.height = videoElement.videoHeight;
-                context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+            const worker = new Worker('jsQR.js');
+            worker.postMessage({ type: 'init' });
 
-                const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            worker.onmessage = (e) => {
+                const result = e.data;
+                if (result.type === 'decoded') {
+                    qrResultElement.textContent = `Resultado del escaneo: ${result.data}`;
 
-                worker.postMessage({ type: 'decode', data: imageData }, [imageData.data.buffer]);
-            }
+                    // Redirige al usuario al enlace del código QR (opcional)
+                    window.location.href = result.data;
+                }
+            };
+
+            const onFrame = async () => {
+                if (videoElement.readyState === videoElement.HAVE_ENOUGH_DATA) {
+                    const canvas = document.createElement('canvas');
+                    const context = canvas.getContext('2d');
+                    canvas.width = videoElement.videoWidth;
+                    canvas.height = videoElement.videoHeight;
+                    context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+
+                    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+                    // Agrega registros para verificar si se capturan los datos de la imagen
+                    console.log('Capturando datos de imagen:', imageData);
+
+                    worker.postMessage({ type: 'decode', data: imageData }, [imageData.data.buffer]);
+                }
+                requestAnimationFrame(onFrame);
+            };
             requestAnimationFrame(onFrame);
-        };
-        requestAnimationFrame(onFrame);
+        }
     } catch (error) {
         console.error('Error al acceder a la cámara:', error);
     }
 }
 
 startQRScanner();
-    document.getElementById("accept-button").addEventListener("click", function() {
-        // Aquí puedes colocar el código que deseas ejecutar cuando se hace clic en el botón "Aceptar".
-        // Por ejemplo, puedes redirigir al usuario a otra página o realizar alguna otra acción.
-        alert("Código QR aceptado");
-    });
+
